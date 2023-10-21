1be8c9399a46cb9ca332f9f25b96d8aedc9b369f
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-10-21 16:03:18 -0700

    Complete Exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,14 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+pp @store1
+pp @store2
+
+@store1.name = "Victoria"
+@store1.save
+
+@store1 = Store.find_by(id: 1)
+pp @store1
