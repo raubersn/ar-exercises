92aae66f21c55c7a34dce7544bb726a233236334
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-10-21 17:03:57 -0700

    Complete Exercise 4

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ source "https://rubygems.org"
 gem 'pry' # for debugging
 gem 'pg'
 gem 'activerecord', '6.1.5.1'
+gem 'money', '6.16.0'
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -2,8 +2,39 @@ require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
+require 'money'
+
+Money.locale_backend = :i18n
+I18n.locale = :en
 
 puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+# Surrey (annual_revenue of 224000, carries women's apparel only)
+Store.create(name: "Surrey",
+            annual_revenue: 224000,
+            mens_apparel: false,
+            womens_apparel: true)
+# Whistler (annual_revenue of 1900000 carries men's apparel only)
+Store.create(name: "Whistler",
+            annual_revenue: 1900000,
+            mens_apparel: true,
+            womens_apparel: false)
+# Yaletown (annual_revenue of 430000 carries men's and women's apparel)
+Store.create(name: "Yaletown",
+            annual_revenue: 430000,
+            mens_apparel: true,
+            womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |store|
+  puts "#{store.name} store's annual revenue: #{Money.from_amount(store.annual_revenue, "CAD").format}"
+end
+
+@low_revenue_womens_stores = Store.where("womens_apparel = ? AND annual_revenue < ?", true, 1000000)
+
+@low_revenue_womens_stores.each do |store|
+  puts "#{store.name} store's has a low annual revenue of #{Money.from_amount(store.annual_revenue, "CAD").format}"
+end
